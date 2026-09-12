db3ad3171b6c9015222e79d6f05ac5b713a9568b
boopathi011 <boopathi10105@gmail.com>
2026-09-12 12:19:16 +0530

    first version of my web app

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1>Hello from my web app</h1>" \
+    "<a href='/about'>About</a>"
+    
+
+@app.route('/about')
+def about():
+    return "<p>Built Locally, pushed via github</p>"
+
+if __name__ =='__main__':
+    app.run(debug=True)
